7cab842d96c423995d64c1c9cd88a59733570af7
CarterTheEvil <linuxuser2014@protonmail.com>
2026-08-28 11:37:13 -0700

    Refactor boot kernel assembly code structure

--- boot_kernal.asm
+++ boot_kernal.asm
@@ -1,45 +1,134 @@
-; Unit-OS boot kernel
-; GRUB Multiboot v1 compatible
-; 32-bit x86
-
 BITS 32
 
 section .multiboot
 align 4
 
 multiboot_header:
-    dd 0x1BADB002                  ; Multiboot magic
-    dd 0x00000003                  ; Flags
-    dd -(0x1BADB002 + 0x00000003)  ; Checksum
+    dd 0x1BADB002
+    dd 0x00000003
+    dd -(0x1BADB002 + 0x00000003)
+
 
 section .text
+
 global _start
+global idt_flush
+global keyboard_isr
+
 extern kernel_main
+extern keyboard_handler
+
 
 _start:
+
     cli
 
-    ; Set up our stack
+    ; Save Multiboot values
+    mov esi, eax
+    mov edi, ebx
+
+    ; Load our GDT
+    lgdt [gdt_descriptor]
+
+    ; Reload code segment
+    jmp 0x08:.reload_segments
+
+.reload_segments:
+
+    mov ax, 0x10
+
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+    ; Setup stack
     mov esp, stack_top
 
-    ; Pass Multiboot information to C
-    ; EAX = magic
-    ; EBX = multiboot info structure
-    push ebx
-    push eax
+    ; Call C kernel
+    push edi
+    push esi
 
     call kernel_main
 
-    ; If kernel_main returns, halt forever
 .hang:
     cli
     hlt
     jmp .hang
 
 
+; ------------------------------------------------
+; Load IDT
+; ------------------------------------------------
+
+idt_flush:
+
+    mov eax, [esp + 4]
+    lidt [eax]
+
+    ret
+
+
+; ------------------------------------------------
+; Keyboard IRQ1
+; ------------------------------------------------
+
+keyboard_isr:
+
+    pusha
+
+    call keyboard_handler
+
+    popa
+
+    iretd
+
+
+; ------------------------------------------------
+; GDT
+; ------------------------------------------------
+
+section .data
+
+gdt_start:
+
+gdt_null:
+    dq 0
+
+gdt_code:
+    dw 0xFFFF
+    dw 0
+    db 0
+    db 10011010b
+    db 11001111b
+    db 0
+
+gdt_data:
+    dw 0xFFFF
+    dw 0
+    db 0
+    db 10010010b
+    db 11001111b
+    db 0
+
+gdt_end:
+
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+
+; ------------------------------------------------
+; Stack
+; ------------------------------------------------
+
 section .bss
+
 align 16
 
 stack_bottom:
-    resb 16384                     ; 16 KB stack
+    resb 16384
+
 stack_top:
