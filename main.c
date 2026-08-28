b98375876bd31814891c04b14cd83320b9343f22
CarterTheEvil <linuxuser2014@protonmail.com>
2026-08-28 11:09:51 -0700

    Initialize main.c with basic shell structure

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+    char input[100]; 
+
+    printf("Welcome to Unit OS\n");
+    printf("------------------\n\n");
+
+    while(1) {
+
+        printf("UnitOS> "); 
+        
+
+        if (fgets(input, sizeof(input), stdin) != NULL) {
+            
+
+        }
+    }
+    
+    return 0;
+}
